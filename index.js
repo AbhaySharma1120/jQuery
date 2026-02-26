a45ef2d2b4ff1394c7de809e3fd4c96d62611fba
AbhaySharma1120 <abhaysharma4762@gmail.com>
2026-02-26 19:18:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$("document").ready(function () {
+  $("button").click(function(){
+
+    //hides all paragraphs
+    // $("p").hide();
+
+    //hides everything
+    // $("*").hide();
+
+    //hides the selector it has instance of
+    // $(this).hide();
+
+    //hides the item with particular id
+    // $("#para4").hide();
+
+    //hides the item matching the class
+    // $(".main").hide();
+
+    // $("div .main").hide();
+
+    // $("div .multi p").hide();
+
+    // $("p.main").hide();
+
+    $("#para4, .main").hide();
+
+  });
+});
