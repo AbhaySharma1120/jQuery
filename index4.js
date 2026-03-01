41df8959565cd273b4481a51defe33fdcbf43f09
AbhaySharma1120 <abhaysharma4762@gmail.com>
2026-03-01 12:31:01 +0530

    additional codes

--- index4.js
+++ index4.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
         // console.log($("body").css("background-color"));
         // console.log($("body").css("background-color", "#32c888"));
         // $("p").css({"background-color": "#32c888"});
-        $("p").css({"background-color": "#32c888", "font-size": "80px"});
+        // $("p").css({"background-color": "#32c888", "font-size": "80px"});
+
+        // $("h1, h2, p").toggleClass("green");
+
+        // $("h1").text("New Text in Heading");
+        $("h1").html("Hey !!! <span style = 'color:#32c888;'>Atulya</span>, How are you !!!");
     });
 });
